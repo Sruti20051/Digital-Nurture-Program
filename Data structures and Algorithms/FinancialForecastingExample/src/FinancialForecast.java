b7b845fb8db333925cbda55e03d711e85bbf1683
Sruti20051 <sruti.23bce20051@vitapstudent.ac.in>
2026-07-15 16:03:17 +0530

    Add files via upload

---
+++ Data structures and Algorithms/FinancialForecastingExample/src/FinancialForecast.java
@@ -0,0 +1,12 @@
+public class FinancialForecast {
+    public static double predictFutureValue(double currentValue, double growthRate, int years) {
+        if (years == 0) {
+            return currentValue;
+        }
+        return predictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1
+        );
+    }
+}
