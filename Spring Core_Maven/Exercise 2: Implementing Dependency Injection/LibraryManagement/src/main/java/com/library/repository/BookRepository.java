96f84986ba332651e39b679c307e7cf2f594590a
Sruti20051 <sruti.23bce20051@vitapstudent.ac.in>
2026-07-16 11:09:44 +0530

    Add files via upload

---
+++ Spring Core_Maven/Exercise 2: Implementing Dependency Injection/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void displayBooks() {
+
+        System.out.println("BookRepository: Displaying all library books.");
+
+    }
+
+}
